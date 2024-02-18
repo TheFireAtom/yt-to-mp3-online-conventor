9440a175dfac42538f3cbea552ca1d3e6afc616e
thefireatom <ivaner1234@yandex.ru>
2024-02-18 09:54:35 +0300

    add url to base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/yt-to-mp3-online-conventor/"
+  base: "https://thefireatom.github.io/yt-to-mp3-online-conventor/"
 })
